2c9111b8091305ae3c8d403289648fd2f58056f9
Kazybek <kazybeknurlanbek@gmail.com>
2025-09-22 21:54:17 +0200

    add: more seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,8 +19,12 @@ laliga = League.create!(name: 'La Liga', country: 'Spain')
 # Создание клубов и их привязка к лигам
 manchester_city = Club.create!(name: 'Manchester City', city: 'Manchester', country: 'England', founded_year: 1880, stadium_name: 'Etihad Stadium', league: premier_league)
 real_madrid = Club.create!(name: 'Real Madrid', city: 'Madrid', country: 'Spain', founded_year: 1902, stadium_name: 'Santiago Bernabéu', league: laliga)
+liverpool = Club.create!(name: 'Liverpool', city: 'Liverpool', country: 'England', founded_year: 1892, stadium_name: 'Anfield', league: premier_league)
+barcelona = Club.create!(name: 'FC Barcelona', city: 'Barcelona', country: 'Spain', founded_year: 1899, stadium_name: 'Camp Nou', league: laliga)
 
 # Создание игроков и их привязка к клубам
 Player.create!(first_name: 'Erling', last_name: 'Haaland', nationality: 'Norway', birth_date: '2000-07-21', position: 'Forward', club: manchester_city)
 Player.create!(first_name: 'Kevin', last_name: 'De Bruyne', nationality: 'Belgium', birth_date: '1991-06-28', position: 'Midfielder', club: manchester_city)
 Player.create!(first_name: 'Kylian', last_name: 'Mbappé', nationality: 'France', birth_date: '1998-12-20', position: 'Forward', club: real_madrid)
+Player.create!(first_name: 'Mohamed', last_name: 'Salah', nationality: 'Egypt', birth_date: '1992-06-15', position: 'Forward', club: liverpool)
+Player.create!(first_name: 'Robert', last_name: 'Lewandowski', nationality: 'Poland', birth_date: '1988-08-21', position: 'Forward', club: barcelona)
